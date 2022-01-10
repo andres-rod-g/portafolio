ebccd0897621ab6b2a982ff10a49ca56d0f9b630
Andrés Rodríguez <79536152+andres-rod-g@users.noreply.github.com>
2022-01-10 11:40:52 -0500

    Client Commit

---
+++ client/src/components/home/home.js
@@ -0,0 +1,9 @@
+import Styles from './style.module.scss'
+
+export default () => {
+    return (
+        <div className={Styles.homeContainer}>
+            <p>HOLA MUNDOOO</p>
+        </div>
+    )
+}
